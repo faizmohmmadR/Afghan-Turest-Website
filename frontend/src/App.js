77ad986144c717760a23794847154e9393b623fe
Faiz Mohammad <faizmohammadramdel@gmail.com>
2023-09-04 15:42:38 +0430

    slider created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./components/layout.js/Header";
 import NavBar from "./components/layout.js/NavBar";
-import { Stack } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
+import Slider from "./components/slider/Slider";
 
 const App = () => {
   return (
     <Stack>
       <Header />
       <NavBar />
+      <Grid>
+        <Slider />
+      </Grid>
     </Stack>
   );
 };
