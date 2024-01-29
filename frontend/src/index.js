3fb02b8f1112aa9920a14b8b653ead282cefd23e
Faiz Mohammad <faizmohammadramdel@gmail.com>
2024-01-29 23:56:54 +0430

    project configuration chaged

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Cards from "./components/layout/Cards";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Slider from "./components/slider/Slider";
-import HistoricalPlaces from "./components/category/HistoricalPlaces";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Slider />,
-      },
-    ],
-  },
-  {
-    path: "/new",
-    element: <HistoricalPlaces />,
-  },
-]);
+const rout = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById("app")).render(
+rout.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
