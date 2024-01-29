7acca5d949fc965058cb34386023a89532d21569
Faiz Mohammad <faizmohammadramdel@gmail.com>
2024-01-29 23:56:54 +0430

    changes added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,12 @@
 import React from "react";
-import { Container, Grid, Stack } from "@mui/material";
-import LeftSidebar from "./components/layout/LeftSidebar";
-import RightSidebar from "./components/layout/RightSidebar";
-import { Outlet } from "react-router-dom";
-import Header from "./components/layout/Header";
-import NavBar from "./components/layout/NavBar";
-import HistoricalPlaces from "./components/category/HistoricalPlaces";
-import Nature from "./components/category/Nature";
-import Parks from "./components/category/Parks";
-import Hotels from "./components/category/Hotels";
-import Restorants from "./components/category/Restorants";
-import Hospitals from "./components/category/Hospitals";
+import NavBar from "./components/layout/navbar/NavBar";
+import { Container } from "@mui/material";
 
 const App = () => {
   return (
-    <Stack>
-      <Header />
+    <Container>
       <NavBar />
-      <Container>
-        <Grid
-          container
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr",
-            gridGap: "10px",
-          }}
-        >
-          <LeftSidebar />
-          <Grid item>
-            <Outlet />
-            <Grid item>
-              <HistoricalPlaces />
-              <Nature />
-              <Parks />
-              <Hotels />
-              <Restorants />
-              <Hospitals />
-            </Grid>
-          </Grid>
-          <RightSidebar />
-        </Grid>
-      </Container>
-    </Stack>
+    </Container>
   );
 };
 
